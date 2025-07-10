696e76afb51e78c86e44f182ef985947e374f4c0
Forest <lin_shirong123@163.com>
2025-07-10 15:30:19 +0800

    随便写一点

--- test.cpp
+++ test.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout<<"Welcome to C++ programming!" << endl;
+    cout << "This is a simple test program." << endl;
     return 0;
 }
